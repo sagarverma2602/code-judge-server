0bcab245c3ca4dcb166910d2810236c21c55d4bb
sagar verma <55575131+sagarverma2602@users.noreply.github.com>
2024-03-21 05:11:38 +0530

    date added

--- api/users/user.services.js
+++ api/users/user.services.js
@@ -4,8 +4,8 @@ const connection =require('../../config/database')
 module.exports={
     create:(data,callback)=>{
         
-        connection.query(`insert into users(id,username,pref_language,stdin,source_code,output,output_status) value(?,?,?,?,?,?,?)`,
-        [data.id,data.username,data.language,data.input,data.code,data.stdout,data.status],
+        connection.query(`insert into users(id,username,pref_language,stdin,source_code,output,output_status,date) value(?,?,?,?,?,?,?,?)`,
+        [data.id,data.username,data.language,data.input,data.code,data.stdout,data.status,new Date()],
         (error,results,fields)=>{
             if(error){
                 return callback(error)
